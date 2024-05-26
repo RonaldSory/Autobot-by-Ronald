d860a45ad8529da8bc107120773afb98e0abe090
RonaldSory <165039163+RonaldSory@users.noreply.github.com>
2024-05-26 18:00:50 +0000

    Create restart.js

---
+++ script/restart.js
@@ -0,0 +1,45 @@
+const fs = require("fs-extra");
+
+module.exports = {
+	config: {
+		name: "restart",
+		version: "1.1",
+		author: "NTKhang",
+		countDown: 5,
+		role: 2,
+		description: {
+			vi: "Khởi động lại bot",
+			en: "Restart bot"
+		},
+		category: "Owner",
+		guide: {
+			vi: "   {pn}: Khởi động lại bot",
+			en: "   {pn}: Restart bot"
+		}
+	},
+
+	langs: {
+		vi: {
+			restartting: "🔄 | Đang khởi động lại bot..."
+		},
+		en: {
+			restartting: "⏳ | 𝗗𝗢𝗡𝗡𝗘 𝗺𝗼𝗶 𝘂𝗻𝗲 𝗺𝗶𝗻𝘂𝘁𝗲𝘀 𝗽𝗼𝘂𝗿 𝗹𝗮 𝗿𝗲́𝗶𝗻𝗰𝗮𝗿𝗻𝗮𝘁𝗶𝗼𝗻......"
+		}
+	},
+
+	onLoad: function ({ api }) {
+		const pathFile = `${__dirname}/tmp/restart.txt`;
+		if (fs.existsSync(pathFile)) {
+			const [tid, time] = fs.readFileSync(pathFile, "utf-8").split(" ");
+			api.sendMessage(`✅ | 𝗺𝗮𝗶𝗻𝘁𝗲𝗻𝗮𝗻𝘁 𝗷'𝗮𝗶 𝗱𝘂𝗿𝗲́ 𝗱𝗲 𝘃𝗶𝗲  \n🕰️ | 𝗧𝗲𝗺𝗽𝘀: ${(Date.now() - time) / 1000}s`, tid);
+			fs.unlinkSync(pathFile);
+		}
+	},
+
+	onStart: async function ({ message, event, getLang }) {
+		const pathFile = `${__dirname}/tmp/restart.txt`;
+		fs.writeFileSync(pathFile, `${event.threadID} ${Date.now()}`);
+		await message.reply(getLang("restartting"));
+		process.exit(2);
+	}
+};
